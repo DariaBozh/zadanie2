1c6d1aca2d138f894f3a12a0bde8b33e4938383d
Daria Bozh <darling.bozh12326@gmail.com>
2025-06-01 20:52:34 +0300

    File base

---
+++ zadanie12-1-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+	
+typedef struct{
+	unsigned int m;
+	unsigned int n;
+	char properties;
+	char *matrix;
+}NURELM;
+	
+char nurelm_set_codomain(NURELM *to_redef, unsigned int size_cod);
+
+int main(){
+	
+	return 0;
+}
