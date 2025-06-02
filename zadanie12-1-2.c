985b8bf70aae647731efee3f596e3690534fca8b
Daria Bozh <darling.bozh12326@gmail.com>
2025-06-02 17:35:14 +0300

    Determining the actual dimensions

--- zadanie12-1-2.c
+++ zadanie12-1-2.c
@@ -2,15 +2,37 @@
 #include <stdlib.h>
 #include <limits.h>
 
-	
+#define SUCCESS 0
+#define FAIL 1
+
+
+// Funkcia zmeni kodomenu relacie tak, aby bola nova mnozina Zsize_cod.
+// Ak sa to neda, vrati FAIL, inac SUCCESS.
+ 
+
 typedef struct{
-	unsigned int m;
-	unsigned int n;
+	unsigned int m;  // домен (рядки)
+	unsigned int n;  // кодомен (стовпці)
 	char properties;
-	char *matrix;
+	char *matrix;   // матриця розміром m * n
 }NURELM;
 	
-char nurelm_set_codomain(NURELM *to_redef, unsigned int size_cod);
+char nurelm_set_codomain(NURELM *to_redef, unsigned int size_cod){
+	unsigned int old_n;
+	unsigned int new_n;
+	unsigned int m;
+	unsigned int i;
+	unsigned int j;
+	unsigned long long total_cells;
+	char *new_matrix;
+	size_t bytes_needed;
+	
+	//актуальні розміри
+	m = to_redef->m;
+	old_n = to_redef->n;
+	new_n = size_cod;
+	
+};
 
 int main(){
 	
